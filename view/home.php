80e01f5e6ceae8113ca2deaf3fce8dab7a774c08
HellSephia <zonezerocrm@gmail.com>
2022-03-28 14:20:09 -0500

    Home description

--- view/home.php
+++ view/home.php
@@ -3,12 +3,24 @@
   <head>
     <title>Title</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="../assets/home-styles.css" />
   </head>
   <body>
       
-      <h1>Hola</h1>
-      
+      <h1 class="home_title">El Consultorio CRUDo.</h1>
+      <div class="home_description"></div>
+        <div class="home_content">
+          En esta pagina podras agendar cualquier cita con nuestro CTO, con el objetivo de solucionar cualquier problema tecnico que tu equipo de desarrollo este teniendo.
+        </div>
+
+        <div class="home_content2">
+        Te recomendamos ir a cada seccion de la nav bar, alli podras crear, editar, eliminar y ver todas tus consultas, asi mismo podras ver aquellas que haz borrado.
+        </div>
+
+        <div class="home_content3">
+          Dudas? Inquietudes? Porfavor contactanos a nuestro e-mail: elconsultoriocrudo@gmail.com.
+        </div>
+      </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
   </body>
