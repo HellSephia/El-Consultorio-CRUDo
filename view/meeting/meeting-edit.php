aa1377fdbec7151980c682b8341f446fd2516ded
Sarah <sarisp3260@gmail.com>
2022-03-28 14:23:34 -0500

    fixing errors

--- view/meeting/meeting-edit.php
+++ view/meeting/meeting-edit.php
@@ -1,14 +1,3 @@
-<html>
-  <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-  </head>
-  <body>
-
-<ol class="breadcrumb d-flex align-items-center gap-3 pt-3">
-  <li><a class="btn btn-primary link" href="?c=Meeting" role="button">Go back</a></li>
-  <li class="active"><?php echo $alm->id != null ? 'Edit meeting of ' . $alm->first_name . ' ' . $alm->last_name : 'Add new meeting'; ?></li>
-</ol>
 
 <form id="frm-alumno" class="mb-5 pb-5 pt-3" action="?c=Meeting&a=Guardar" method="post" enctype="multipart/form-data" >
 <h1 class="page-header pb-2">
@@ -53,9 +42,3 @@
         <?php echo $alm->id != null ? '' : '<input type="reset" value="Reset" class="btn btn-secondary"/>'; ?>
     </div>
 </form>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
-  </body>
-</html>
